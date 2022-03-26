1270058aa01eda3121ea346cbff42521e86c1aec
Lisieux Almeida <lisieux.almeida@gmail.com>
2022-03-26 22:20:16 +0000

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import App from './App';
+import { render, screen } from '@testing-library/react';
+
+describe('Componente principal', () => {
+  describe('Quando eu abro o app do banco', () => {
+    it('Mostrar o nome do banco', () => {
+      render(<App />);
+
+      expect(screen.getByText('ByteBank')).toBeInTheDocument();
+    });
+    test('o saldo é exibido', () => {
+      render(<App />);
+
+      expect(screen.getByText('Saldo:')).toBeInTheDocument();
+    });
+    it('o botão realizar transacao é exibio', () => {
+      render(<App />);
+
+      expect(screen.getByText('Realizar opera')).toBeInTheDocument();
+    });
+  });
+});
